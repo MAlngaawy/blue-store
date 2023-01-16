834a065dc97ea5296f55c9563b8430aa4301b8f5
MAlngaawy <m.alngaawy@gmail.com>
2023-01-16 16:43:37 +0200

    add pritter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="App flex justify-center items-center h-screen">
-      <h1 className='text-3xl font-bold text-center text-red-400'>Hello</h1>
+      <h1 className="text-3xl font-bold text-center text-red-400">Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
