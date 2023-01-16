6b41e4febd6540efd1b4baff7028ef7a3236f862
MAlngaawy <m.alngaawy@gmail.com>
2023-01-16 17:19:32 +0200

    init redux and create counter Slice with it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./app/counter/counterSlice";
+import { RootState } from "./app/store";
+
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold text-center text-red-400">Hello</h1>
+    <div className="App flex flex-col gap-6 justify-center items-center h-screen">
+      <div className="flex gap-2">
+        <div
+          onClick={() => dispatch(increment())}
+          className="counter p-1 text-xl bg-blue-500 text-white rounded-sm cursor-pointer"
+        >
+          increment
+        </div>
+        <div
+          onClick={() => dispatch(decrement())}
+          className="counter p-1 text-xl bg-red-500 text-white rounded-sm cursor-pointer"
+        >
+          Decrement
+        </div>
+      </div>
+      <div className="bg-green-600 p-4 font-bold text-white text-3xl rounded-sm">
+        {count}
+      </div>
     </div>
   );
 }
---
+++ src/app/counter/counterSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 10,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value++;
+    },
+    decrement: (state) => {
+      state.value--;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
